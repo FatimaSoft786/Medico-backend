3badb9cc636917fd68d02817244cee5631903270
shahzaib65 <34715079+shahzaib65@users.noreply.github.com>
2024-02-09 12:53:06 +0500

    Forgot password is added

--- Controller/User.js
+++ Controller/User.js
@@ -1,4 +1,3 @@
-
 const bcrypt = require("bcryptjs")
 const model = require("../Model/User");
 const generateOTP = require("../service/generateOtp");
@@ -62,7 +61,7 @@ const login = async(req,res)=>{
      }
      const comparePass = await bcrypt.compare(req.body.password,user.password);
      if(!comparePass){
-        return res.stat(400).json("Please try to login with correct credentials");
+        return res.status(400).json("Please try to login with correct credentials");
      }
      res.status(200).json(user._id);
 
@@ -88,7 +87,7 @@ const verifyOtp = async(req,res)=>{
                     {_id: user._id},
                     {$set: {otp: ""}},
                     {new: true});
-                    res.status(200).json(data)
+                    res.status(200).json(data);
             }
         }
     } catch (error) {
@@ -96,8 +95,63 @@ const verifyOtp = async(req,res)=>{
     }
 };
 
+const checkEmail = async(req,res)=>{
+    try {
+        
+     const user = await model.findOne({email: req.body.email});
+     if(!user){
+        return res.status(400).json("User with this email does not exist")
+     }else {
+        const otp = generateOTP();
+           await model.findByIdAndUpdate(
+                    {_id: user._id},
+                    {$set: {otp: otp}},
+                    {new: true});
+          let mailOption = {
+                from: process.env.SMTP_MAIL,
+                to: req.body.email,
+                subject: "Otp verification",
+                text: `Your otp is: ${otp} will expire after 10 minute`
+            };
+            transporter.sendMail(mailOption,function(error){
+          if(error){
+            res.status(404).json(error)
+          }else{
+            res.status(200).json("Please check your email ")
+          }
+            });          
+     }
+
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+const forgotPassword = async(req,res)=>{
+    try {
+      
+        const data = await model.findOne({ _id: req.body.id });
+    console.log(data);
+    const salt = await bcrypt.genSalt(10);
+    const securePass = await bcrypt.hash(req.body.password, salt);
+     const userData = await model.findByIdAndUpdate(
+      { _id: data._id },
+      { $set: { password: securePass } },
+      { new: true }
+    );
+    res.status(200).json("Your password has been updated");
+
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+
+
 module.exports = {
     register,
     login,
-    verifyOtp
+    verifyOtp,
+    checkEmail,
+    forgotPassword
 }
--- Router/User.js
+++ Router/User.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {register,login,verifyOtp} = require("../Controller/User");
-const { verify } = require("jsonwebtoken");
+const {register,login,verifyOtp,checkEmail,forgotPassword} = require("../Controller/User");
 const router = express.Router();
 router.post("/register",register);
 router.post("/login",login);
 router.post("/verify",verifyOtp);
+router.post("/checkEmail",checkEmail);
+router.put("/forgot",forgotPassword);
 module.exports = router;
